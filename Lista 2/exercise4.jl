387db8687dd499344c3d3df9198ba8704da23a18
Piotr Zapała <piotrekapala25@gmail.com>
2022-11-13 00:50:39 +0100

    fix : fixed some bugs

--- Lista 2/exercise4.jl
+++ Lista 2/exercise4.jl
@@ -1,8 +1,8 @@
 using Polynomials
 
 function polynomials(coefficient)
-    coefficientFormOfPolynomial = Polynomial(reverse(coefficient))
-    productFormOfPolynomial = fromroots(1:20)
+    coefficientFormOfPolynomial = Polynomial(coefficient)
+    productFormOfPolynomial = fromroots(1.0:20.0)
     RootsOfCoefficientForm = roots(coefficientFormOfPolynomial)
     RootsOfProductForm = roots(productFormOfPolynomial)
     return coefficientFormOfPolynomial, productFormOfPolynomial, RootsOfCoefficientForm, RootsOfProductForm
@@ -12,7 +12,7 @@ function calculateTheValue(coefficientFormOfPolynomial, productFormOfPolynomial,
     p1 = coefficientFormOfPolynomial
     p2 = productFormOfPolynomial
     r1 = RootsOfCoefficientForm
-    r2 = collect(1:20)
+    r2 = collect(1.0:20.0)
     valuesOfCoefficientForm = map(x -> abs(p1(x)), r1)
     valuesOfProductForm = map(x -> abs(p2(x)), r1)
     relativeError = map((x, y) -> abs(x - y), r1, r2)
@@ -37,13 +37,16 @@ function exercise(coefficient)
 end
 
 function main()
-    coefficientA = [1, -210.0, 20615.0, -1256850.0,
+    coefficientA = reverse([1, -210.0, 20615.0, -1256850.0,
         53327946.0, -1672280820.0, 40171771630.0, -756111184500.0,          
         11310276995381.0, -135585182899530.0, 1307535010540395.0, -10142299865511450.0, 
         63030812099294896.0, -311333643161390640.0, 1206647803780373360.0, -3599979517947607200.0,
-        8037811822645051776.0, -12870931245150988800.0, 13803759753640704000.0, -8752948036761600000.0, 2432902008176640000.0]
-    coefficientB = coefficientA
-    coefficientB[2] = -210.0-(2^(-23))
+        8037811822645051776.0, -12870931245150988800.0, 13803759753640704000.0, -8752948036761600000.0, 2432902008176640000.0])
+    coefficientB = reverse([1,  -210.0-2^(-23), 20615.0, -1256850.0,
+        53327946.0, -1672280820.0, 40171771630.0, -756111184500.0,          
+        11310276995381.0, -135585182899530.0, 1307535010540395.0, -10142299865511450.0, 
+        63030812099294896.0, -311333643161390640.0, 1206647803780373360.0, -3599979517947607200.0,
+        8037811822645051776.0, -12870931245150988800.0, 13803759753640704000.0, -8752948036761600000.0, 2432902008176640000.0])
     exercise(coefficientA)
     println()
     exercise(coefficientB)
